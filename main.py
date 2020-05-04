dbffe2a1b596b267ff21e4f1ae4b78247bc5ed67
Azamat <bairamaza1975@gmail.com>
2020-05-05 00:32:17 +0500

    Мелкие изменения для боевого запуска

--- main.py
+++ main.py
@@ -648,7 +648,8 @@ def main_page():
 
 # Функция запуска программы
 def main():
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
 # Функция для изменения страницы ошибки 404
