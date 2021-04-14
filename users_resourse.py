736c13ca49cf40647cc308c28b54a2893c640c41
Azamat Bayramov <bayramov.azamat04@gmail.com>
2021-04-14 22:17:20 +0500

    Refactor code in API module

--- users_resourse.py
+++ users_resourse.py
@@ -1,13 +1,15 @@
+"""API module"""
+
 from flask_restful import abort, Resource
 from data import db_session
 from data.__all_models import users
 from flask import jsonify
 
-# Извлечение класса
+# Class extracting
 User = users.User
 
 
-# Функция выдачи ошибки при отсутствии пользователя с таким ID
+# Function for abort, if user with id not found
 def abort_if_user_not_found(user_id):
     session = db_session.create_session()
     user = session.query(User).filter(User.id == user_id).all()
@@ -15,21 +17,40 @@ def abort_if_user_not_found(user_id):
         abort(404, message=f"User {user_id} not found")
 
 
-# Класс API для получения одного пользователя
+# API class for getting one user
 class UsersResource(Resource):
     def get(self, user_id):
         abort_if_user_not_found(user_id)
         session = db_session.create_session()
         user = session.query(User).filter(User.id == user_id).first()
-        return jsonify({'user': user.to_dict(only=('id', 'login', 'registration_date', 'description',
-                                                   'private_account', 'avatar_photo_id'))})
+        return jsonify(
+            {'user': user.to_dict(
+                only=('id',
+                      'login',
+                      'registration_date',
+                      'description',
+                      'private_account',
+                      'avatar_photo_id')
+            )}
+        )
 
 
-# Класс API для получения всех пользователей
+# API class for getting all users
 class UsersListResource(Resource):
     def get(self):
         session = db_session.create_session()
         users = session.query(User).all()
-        return jsonify({'users': [user.to_dict(only=('id', 'login', 'registration_date',
-                                                     'description', 'private_account',
-                                                     'avatar_photo_id')) for user in users]})
+        return jsonify(
+            {'users': [
+                user.to_dict(
+                    only=(
+                        'id',
+                        'login',
+                        'registration_date',
+                        'description',
+                        'private_account',
+                        'avatar_photo_id'
+                    )
+                ) for user in users
+            ]}
+        )
