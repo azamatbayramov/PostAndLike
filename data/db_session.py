f69cfc9af292c6a1f23ca55d15cb72aafd3d88cf
Azamat Bayramov <bayramov.azamat04@gmail.com>
2021-04-14 21:40:08 +0500

    Refactor code

--- data/db_session.py
+++ data/db_session.py
@@ -1,14 +1,19 @@
+"""Module for working with database sessions"""
+
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
+
+# This is a module for working with SQLAlchemy
+# - database initialization and session creation
 
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
 
-# Функция инициальизации базы данных
+# Database initialization function
 def global_init(db_file):
     global __factory
 
@@ -16,10 +21,10 @@ def global_init(db_file):
         return
 
     if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
+        raise Exception("You must specify the database file.")
 
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-    print(f"Подключение к базе данных по адресу {conn_str}")
+    print(f"Connecting to the database at {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
@@ -29,7 +34,7 @@ def global_init(db_file):
     SqlAlchemyBase.metadata.create_all(engine)
 
 
-# Функция создания сессии
+# Session creation function
 def create_session() -> Session:
     global __factory
     return __factory()
